e4d5e52ecf8268a3d31811f751b6ed6f02fa6afa
RynoTheDino <119882072+RynoTheDino@users.noreply.github.com>
2022-12-06 13:57:00 -0700

    Add files via upload
    Creates variables and bulk uploads users via selected CSV file. Adds the Surname 'Test' to all users for identification and adds them to a newly created Test Group called 'Test Import Group.'
    
    Commented out at the end is a simple deletion scrip will remove the users and testgroup from your Azure account.

---
+++ TestGroup_BulkImport.ps1
@@ -0,0 +1,38 @@
+﻿$domain = Get-AzureADDomain
+$DName = $domain.name
+$PswdProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+
+#CHANGE PASSWORD BELOW!
+
+$PswdProfile.Password = "D3f@ultPa5sw0rd!!"
+
+New-AzureADGroup -Description 'Test Import Group' -MailEnabled 0 -SecurityEnabled 1 -DisplayName 'Test Group' -MailNickName None
+$Testgroup = Get-AzureADGroup | Where-Object {$_.displayname -eq 'Test Group'}
+
+$bulkfile = Read-Host "enter filepath"
+$bulk = Import-CSV $bulkfile
+
+ForEach ($u in $bulk) {
+    $Short = $u.First
+    $display = $u.First + " " + $u.Last
+    Write-Host "Creating $display"
+    New-AzureADUser -DisplayName $display -UserPrincipalName $Short@$DName -AccountEnabled $true -PasswordProfile $PswdProfile -MailNickName $Short -Surname 'Test'
+  }
+
+$TestMembers = Get-AzureADUser | Where-Object {$_.surname -eq 'Test'}
+
+foreach ($tuser in $TestMembers) {
+    Add-AzureADGroupMember -ObjectId $Testgroup.ObjectId -RefObjectId $tuser.ObjectId
+    Write-Host "Adding Test Surname Users to Test Group"
+  }
+
+#### Run Below to clear users and group###
+
+
+#foreach ($delU in $TestMembers) {
+#    Remove-AzureADUser -ObjectId $delU.ObjectId
+#  }
+#
+#  foreach ($delG in $Testgroup) {
+#    Remove-AzureADGroup -ObjectId $delG.ObjectId
+#  }
